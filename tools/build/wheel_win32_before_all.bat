ae12cc90f488d82ae5cae41ef404d99eae7923eb
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2021-11-12 14:56:35 +0100

    Take a look at what pg_config has to say...

--- tools/build/wheel_win32_before_all.bat
+++ tools/build/wheel_win32_before_all.bat
@@ -2,5 +2,8 @@
 pip install delvewheel
 choco upgrade postgresql
 
-type C:\ProgramData\chocolatey\logs\chocolatey.log
-dir "C:\program files\PostgreSQL"
+:: type C:\ProgramData\chocolatey\logs\chocolatey.log
+:: Infinite long, but it reports PG 14.0.1 installed in the past days
+:: dir "C:\program files\PostgreSQL"
+:: There's only 14
+pg_config
